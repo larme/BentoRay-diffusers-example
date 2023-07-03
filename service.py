313cf065782b5cf55b723f9a6b427a841a833aea
Zhao Shenyang <dev@zsy.im>
2023-07-04 03:02:16 +0800

    convert sync endpoint to async endpoint

--- service.py
+++ service.py
@@ -9,8 +9,8 @@ sd21_runner = bento_model.to_runner(name="sd21-runner")
 svc = bentoml.Service("stable-diffusion-21", runners=[sd21_runner])
 
 @svc.api(input=JSON(pydantic_model=SDArgs), output=Image())
-def txt2img(input_data):
+async def txt2img(input_data):
     kwargs = input_data.dict()
-    res = sd21_runner.run(**kwargs)
+    res = await sd21_runner.async_run(**kwargs)
     images = res[0]
     return images[0]
