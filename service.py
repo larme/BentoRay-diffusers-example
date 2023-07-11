d511acb001644a9d37eecc8708d555fa166f21bc
Zhao Shenyang <dev@zsy.im>
2023-07-11 15:47:02 +0800

    remove additionalProp1 field from swagger UI

--- service.py
+++ service.py
@@ -8,7 +8,9 @@ sd21_runner = bento_model.to_runner(name="sd21-runner")
 
 svc = bentoml.Service("stable-diffusion-21", runners=[sd21_runner])
 
-@svc.api(input=JSON(pydantic_model=SDArgs), output=Image())
+input_spec = JSON.from_sample(SDArgs(prompt="a bento box"))
+
+@svc.api(input=input_spec, output=Image())
 async def txt2img(input_data):
     kwargs = input_data.dict()
     res = await sd21_runner.async_run(**kwargs)
